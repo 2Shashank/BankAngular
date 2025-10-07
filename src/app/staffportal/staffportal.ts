94738d927b408e7d110cfca21f5bc635c565c200
sandeepkumar2407 <sandeep240704@gmail.com>
2025-10-08 01:58:54 +0530

    validations

--- src/app/staffportal/staffportal.ts
+++ src/app/staffportal/staffportal.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Apiservice } from '../apiservice';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'bbb-staffportal',
@@ -7,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './staffportal.css'
 })
 export class Staffportal {
+constructor(private api:Apiservice,private router:Router){}
+  StaffLogout() {
+    const confirmLogout = confirm("Are you sure you want to logout?");
+  
+    if (!confirmLogout) {
+      return;
+    }
 
+    this.api.logout().subscribe({
+      next: (res:any) => {
+        sessionStorage.clear();
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        console.error("Error logging out:", err);
+        alert("Error logging out. Please try again.");
+      }
+    });
+  }
 }
